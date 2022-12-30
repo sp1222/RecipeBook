fa6c3f7f4f1017c05fd838d13cc8927dc7e2b3b2
sp1222 <s.polozoff@gmail.com>
2022-12-30 14:39:24 -0600

    add recipes blueprint to app.py

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ My Personal Website
 from flask import Flask
 from src.blueprints.home_blueprint import home_blueprint
 from src.blueprints.ingredients.ingredients_blueprint import ingredients_blueprint
+from src.blueprints.recipes.recipes_blueprint import recipes_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(home_blueprint)
 app.register_blueprint(ingredients_blueprint)
+app.register_blueprint(recipes_blueprint)
 
 if __name__ == '__main__':
     print('booting up Recipe Book')
