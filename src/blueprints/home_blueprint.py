a919d27d63f1ba9cf41ff6fdd1b7c16d101016dd
sp1222 <s.polozoff@gmail.com>
2023-01-16 14:24:22 -0600

    rearranged code

--- src/blueprints/home_blueprint.py
+++ src/blueprints/home_blueprint.py
@@ -8,6 +8,7 @@ home_blueprint = Blueprint('rb_home', __name__, template_folder='../../templates
 
 
 @home_blueprint.route('/')
+@home_blueprint.route('/home')
 def home():
     '''
     Recipe Book Home Page endpoint
